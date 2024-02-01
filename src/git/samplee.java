24480cc146154a3ff4b8f53ddc88b7092706e4d8
AshokElancheliyan <ashokelancheliyan@gmail.com>
2024-02-01 14:15:46 +0530

    gfg

--- src/git/samplee.java
+++ src/git/samplee.java
@@ -4,7 +4,7 @@ public class samplee {
 
 	public void ask() {
 		System.out.println("Reception");
-		System.out.println("medjygj");
+		System.out.println("medical");
 	}
 
 	public static void main(String[] args) {
