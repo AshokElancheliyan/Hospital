4af7c6cebe032ee3a8c723e49666661d1d98369b
AshokElancheliyan <ashokelancheliyan@gmail.com>
2024-02-01 14:14:30 +0530

    jhg:

--- src/git/samplee.java
+++ src/git/samplee.java
@@ -5,6 +5,7 @@ public class samplee {
 	public void ask() {
 		System.out.println("Reception");
 		System.out.println("medjygj");
+		system.out.println("test");
 	}
 
 	public static void main(String[] args) {
